88bfe0f74b8509a45d9d60a20da0644478d39d12
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-10-14 19:09:48 -0300

    Requisito 17 , 18 e 19

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const botaoEntrar = document.getElementsByClassName('entrar-btn')[0];
 const email = document.querySelector('.email');
 const senha = document.querySelector('.senha');
+const botaoEnviar = document.getElementById('submit-btn');
+const checar = document.getElementById('agreement');
 
 function login() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -9,5 +11,16 @@ function login() {
     window.alert('Email ou senha inválidos.');
   }
 }
-
 botaoEntrar.addEventListener('click', login);
+
+botaoEnviar.disabled = true;
+
+function check() {
+  if (checar.checked === true) {
+    botaoEnviar.disabled = false;
+  } else {
+    botaoEnviar.disabled = true;
+  }
+}
+
+checar.addEventListener('click', check);
